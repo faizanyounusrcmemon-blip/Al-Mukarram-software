ed9a3767304f82c2377076f4c068c63914e6aae2
faizanyounusrcmemon-blip <faizanyounusrcmemon@gmail.com>
2026-07-31 12:47:38 +0500

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -75,7 +75,7 @@ export default function Navbar({ onNavigate }) {
   return (
     <nav className="vip-navbar">
       <div className="nav-logo" onClick={() => go("dashboard")}>
-        ✈ AL MUKARRAM Travel & TOURS
+        ✈ AL MUKARRAM TRAVEL & TOURS
       </div>
 
       <div className="nav-links">
